5187d114d2b89e6d064c610906e8fb3f70b765d8
Quang <quangtd.24it@gmail.com>
2021-11-13 02:43:32 +0700

    fix book and clothes

--- src/controller/clothesDAO/ClothesItemDAO.java
+++ src/controller/clothesDAO/ClothesItemDAO.java
@@ -1,4 +1,22 @@
 package controller.clothesDAO;
 
+import java.util.List;
+
+import model.clothes.ClothesItem;
+
 public interface ClothesItemDAO {
+	
+	void addClothesItem();
+
+	void deleteClothesItem();
+
+	public void updateClothesItem(int ID, int numbers);
+	
+	public List <ClothesItem> findAllShortItem();
+	
+	public List <ClothesItem> findAllJeansItem();
+	
+	public List <ClothesItem> findAllShirtItem();
+	
+	public ClothesItem getClothesItemByID(int ID);
 }
